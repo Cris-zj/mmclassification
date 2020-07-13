574c77b4d23096b00ebd342f94bc28b41d6bce31
jiaozhu <jiaozhu@njust.edu.cn>
2020-07-13 23:23:47 +0800

    add osnet

--- mmcls/models/backbones/osnet.py
+++ mmcls/models/backbones/osnet.py
@@ -85,9 +85,9 @@ class OSBlock(nn.Module):
                  conv_cfg=None,
                  norm_cfg=dict(type='BN'),
                  act_cfg=dict(type='ReLU')):
-        self.downsample = downsample
         self.inplanes = inplanes
         self.planes = planes
+        self.downsample = downsample
         self.conv_cfg = conv_cfg
         self.norm_cfg = norm_cfg
         self.act_cfg = act_cfg
@@ -119,7 +119,8 @@ class OSBlock(nn.Module):
             multi_streams.append(nn.Sequential(*layers))
         self.multi_streams = nn.ModuleList(multi_streams)
 
-        self.fuse_gate = ChannelGate(mid_planes)
+        self.gate_fuse = ChannelGate(mid_planes)
+
         self.conv1x1_last = ConvModule(
             in_channels=self.inplanes,
             out_channels=self.planes,
@@ -131,12 +132,26 @@ class OSBlock(nn.Module):
             act_cfg=None
         )
 
+        self.relu = nn.ReLU(inplace=True)
+
     def forward(self, x):
         identity = x
 
         x1 = self.conv1x1_first(x)
-        
 
+        out = self.gate_fuse(self.multi_streams[0](x1))
+        for i in range(1, self.expansion):
+            out += self.gate_fuse(self.multi_streams[i](x1))
+        
+        out = self.conv1x1_last(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
 
 
 @BACKBONES.register_module()
@@ -176,13 +191,38 @@ class OSNet(BaseBackbone):
 
         self.inplanes = round(64 * self.widen_factor)
         self._make_stem_layer()
-
+        self.block = OSBlock
         self.stage_blocks = self.arch_settings[widen_factor]
-        self.stage_repeats = [2, 2, 2, 1]
-        for i, n in enumerate(self.stage_repeats):
-            outplanes = self.stage_blocks[i]
-            os_layer = make_layer(outplanes, n)
+        self.stage_repeats = [2, 2, 2]
+        self.os_layers = []
+        for i, num_blocks in enumerate(self.stage_repeats):
+            outplanes = self.stage_blocks[i + 1]
+            reduce_spatial_size = True if i < 2 else False
+            os_layer = self.make_os_layer(
+                self.block,
+                self.inplanes,
+                outplanes,
+                num_blocks,
+                reduce_spatial_size)
             self.inplanes = outplanes
+            layer_name = f'layer{i + 1}'
+            self.add_module(layer_name, os_layer)
+            self.os_layers.append(layer_name)
+        layer = ConvModule(
+            in_channels=self.inplanes,
+            out_channels=self.inplanes,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            conv_cfg=self.conv_cfg,
+            norm_cfg=self.norm_cfg,
+            act_cfg=act_cfg=self.act_cfg
+        )
+        layer_name = f'layer{len(self.stage_repeats) + 1}'
+        self.add_module(layer_name, layer)
+        self.os_layers.append(layer_name)
+
+        self._freeze_stages()
 
     def _make_stem_layer(self):
         self.conv1 = ConvModule(
@@ -197,10 +237,61 @@ class OSNet(BaseBackbone):
         )
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-    def make_layer(self, out_channels, num_blocks, block):
+    def make_os_layer(self,
+                      block,
+                      in_channels,
+                      out_channels,
+                      num_blocks,
+                      reduce_spatial_size):
+        downsample = ConvModule(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            conv_cfg=self.conv_cfg,
+            norm_cfg=self.norm_cfg,
+            act_cfg=None
+        )
         layers = []
-        layers.append(block())
-        # for i in range(num_blocks):
+        layers.append(
+            block(
+                inplanes=in_channels,
+                planes=out_channels,
+                downsample=downsample,
+                conv_cfg=self.conv_cfg,
+                norm_cfg=self.norm_cfg,
+                act_cfg=self.act_cfg
+            )
+        )
+        for i in range(1, num_blocks):
+            layers.append(
+                block(
+                    inplanes=out_channels,
+                    planes=out_channels,
+                    downsample=None,
+                    conv_cfg=self.conv_cfg,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg
+                )
+            )
+        if reduce_spatial_size:
+            layers.append(
+                nn.Sequential(
+                    ConvModule(
+                        in_channels=out_channels,
+                        out_channels=out_channels,
+                        kernel_size=1,
+                        stride=1,
+                        padding=0,
+                        conv_cfg=self.conv_cfg,
+                        norm_cfg=self.norm_cfg,
+                        act_cfg=self.act_cfg
+                    ),
+                    nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
+                )
+            )
+        return nn.Sequential(*layers)
 
     def _freeze_stages(self):
         if self.frozen_stages >= 0:
@@ -224,3 +315,12 @@ class OSNet(BaseBackbone):
                     constant_init(m, 1)
         else:
             raise TypeError('pretrained must be a str or None')
+    
+    def train(self, mode=True):
+        super(OSNet, self).train(mode)
+        self._freeze_stages()
+        if mode and self.norm_eval:
+            for m in self.modules():
+                # trick: eval have effect on BatchNorm only
+                if isinstance(m, _BatchNorm):
+                    m.eval()
