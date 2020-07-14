1ada927d8e1a37a0eca8485fd4cc596bf213388c
jiaozhu <jiaozhu@njust.edu.cn>
2020-07-14 17:40:10 +0800

    add backbones

--- mmcls/models/backbones/__init__.py
+++ mmcls/models/backbones/__init__.py
@@ -1,7 +1,9 @@
 from .alexnet import AlexNet
+from .darknet import DarkNet
 from .lenet import LeNet5
 from .mobilenet_v2 import MobileNetV2
 from .mobilenet_v3 import MobileNetv3
+from .osnet import OSNet
 from .regnet import RegNet
 from .resnet import ResNet, ResNetV1d
 from .resnet_cifar import ResNet_CIFAR
@@ -14,5 +16,5 @@ from .shufflenet_v2 import ShuffleNetV2
 __all__ = [
     'LeNet5', 'AlexNet', 'RegNet', 'ResNet', 'ResNeXt', 'ResNetV1d',
     'ResNetV1d', 'ResNet_CIFAR', 'SEResNet', 'SEResNeXt', 'ShuffleNetV1',
-    'ShuffleNetV2', 'MobileNetV2', 'MobileNetv3'
+    'ShuffleNetV2', 'MobileNetV2', 'MobileNetv3', 'OSNet', 'DarkNet'
 ]
