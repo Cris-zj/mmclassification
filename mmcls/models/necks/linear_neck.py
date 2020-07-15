d6d5897d16fc160d73c3c119bd406b6aec365985
jiaozhu <jiaozhu@njust.edu.cn>
2020-07-15 15:40:44 +0800

    fix flake8

--- mmcls/models/necks/linear_neck.py
+++ mmcls/models/necks/linear_neck.py
@@ -15,16 +15,16 @@ class LinearNeck(nn.Module):
         self.in_channels = in_channels
         self.out_channels = out_channels
         self._init_layers()
-    
+
     def _init_layers(self):
         self.fc = nn.Linear(self.in_channels, self.out_channels)
         self.bn = nn.BatchNorm1d(self.out_channels)
         self.relu = nn.ReLU(inplace=True)
-    
+
     def init_weights(self):
         normal_init(self.fc, mean=0, std=0.01, bias=0)
         constant_init(self.bn, 1)
-    
+
     def forward(self, inputs):
         if isinstance(inputs, tuple):
             outs = tuple([self.relu(self.bn(self.fc(x))) for x in inputs])
@@ -36,5 +36,3 @@ class LinearNeck(nn.Module):
         else:
             raise TypeError('neck inputs should be tuple or torch.tensor')
         return outs
-
-
