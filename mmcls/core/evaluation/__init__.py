8517507ec4f0a2556051e600885d261029fa140c
jiaozhu <jiaozhu@njust.edu.cn>
2020-07-15 15:39:47 +0800

    add evaluation compare distance

--- mmcls/core/evaluation/__init__.py
+++ mmcls/core/evaluation/__init__.py
@@ -1,3 +1,7 @@
+from .distance import compute_distance_matrix
 from .eval_hooks import DistEvalHook, EvalHook
 
-__all__ = ['DistEvalHook', 'EvalHook']
+__all__ = [
+    'compute_distance_matrix',
+    'DistEvalHook', 'EvalHook'
+]
