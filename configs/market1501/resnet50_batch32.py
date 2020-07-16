ffff91f72906d53160d8b28b3bf77a5bae0bbdfa
jiaozhu <jiaozhu@njust.edu.cn>
2020-07-16 14:47:28 +0800

    change config name

--- configs/market1501/resnet50_batch32.py
+++ configs/market1501/resnet50_batch32.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/resnet50_market1501_fc512.py',
+    '../_base_/models/resnet50_reid_fc512.py',
     '../_base_/datasets/market1501_bs32.py',
     '../_base_/schedules/market1501_bs32_60e.py',
     '../_base_/default_runtime.py'
