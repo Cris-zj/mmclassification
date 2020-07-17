4c915b511f7e182e94c9b033ce99b86692588956
jiaozhu <jiaozhu@njust.edu.cn>
2020-07-17 16:28:13 +0800

    add schedules configs

---
+++ configs/_base_/schedules/market1501_bs64_60e.py
@@ -0,0 +1,7 @@
+# optimizer
+optimizer = dict(type='Adam', lr=0.0006, weight_decay=5e-4,
+                 betas=(0.9, 0.999), amsgrad=True)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(policy='step', step=[20])
+total_epochs = 60
