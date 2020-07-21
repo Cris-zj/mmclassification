b719695c55038fa80dda9ae2bca2bc3a45387f1f
jiaozhu <jiaozhu@njust.edu.cn>
2020-07-21 19:03:36 +0800

    modify osnet config

--- configs/market1501/osnet_1.0_batch64.py
+++ configs/market1501/osnet_1.0_batch64.py
@@ -2,5 +2,19 @@ _base_ = [
     '../_base_/models/osnet_1.0_reid_fc512.py',
     '../_base_/datasets/market1501_bs64.py',
     '../_base_/schedules/market1501_bs64_250e_coslr.py',
-    '../_base_/default_runtime.py'
 ]
+# checkpoint saving
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=100,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = 'pretrained/osnet_x1_0_imagenet.pth'
+resume_from = 'work_dirs/osnet_1.0_batch64/epoch_10.pth'
+workflow = [('train', 1)]
