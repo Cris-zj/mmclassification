731dc8516caa56488283757f500708f8d2f4bf53
jiaozhu <jiaozhu@njust.edu.cn>
2020-08-12 09:36:51 +0800

    add linear soft head to support soft loss

--- mmcls/models/heads/__init__.py
+++ mmcls/models/heads/__init__.py
@@ -1,4 +1,5 @@
 from .cls_head import ClsHead
 from .linear_head import LinearClsHead
+from .linear_soft_head import LinearSoftClsHead
 
-__all__ = ['ClsHead', 'LinearClsHead']
+__all__ = ['ClsHead', 'LinearClsHead', 'LinearSoftClsHead']
